f8c9ffceac87d92b4a4f949991c0df4428e8ce74
dswtzer <dschw3@illinois.edu>
2020-08-03 12:53:56 -0500

    Adding orginal svg files

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/aitsdev/Desktop/APPS/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/aitsdev/Desktop/PROJECTS/bsadosecalculator"
-export "FLUTTER_TARGET=/Users/aitsdev/Desktop/PROJECTS/bsadosecalculator/lib/main.dart"
+export "FLUTTER_ROOT=/Users/aitsdev/desktop/APPS/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/aitsdev/Desktop/PROJECTS/mob-vetbsa"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/aitsdev/Desktop/APPS/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=10"
-export "TRACK_WIDGET_CREATION=true"
+export "FLUTTER_FRAMEWORK_DIR=/Users/aitsdev/desktop/APPS/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_BUILD_NAME=1.0.1"
+export "FLUTTER_BUILD_NUMBER=11"
